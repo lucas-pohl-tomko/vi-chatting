c3c9424d029fb2723f615d905720ae8d427fb486
Lucas Tomkow <49009354+lucas-pohl-tomko@users.noreply.github.com>
2023-06-19 23:57:15 -0300

    .

--- src/layouts/Home.vue
+++ src/layouts/Home.vue
@@ -5,7 +5,6 @@
                 Hello! This is your home Layout! Have Fun!
             </div>
             <div class="flex justify-center text 2xl font-semibold text-white">
-                
                 <p><a target="_blank" href="https://vuetifyjs.com/en/features/application-layout/">https://vuetifyjs.com/en/features/application-layout/</a></p>
             </div>
             <div class="flex justify-center text 2xl font-semibold text-white">
