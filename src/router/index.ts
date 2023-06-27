aebdface4ae5fee1053a9f26d733acd906ac8a4a
lucas.tomko <lucas.tomko@dinamio.com.br>
2023-06-27 16:36:48 -0300

    fixing wrong import

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { useAuth, currentUser } from "../firebase"
-const { user, isLogin, isLogged } = useAuth()
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
