aa13a63467fbdba0ec4be6d3e94ee560fccebe52
Lucas Tomkow <49009354+lucas-pohl-tomko@users.noreply.github.com>
2023-06-28 17:10:35 -0300

    drawer for mobile

--- src/layouts/Home.vue
+++ src/layouts/Home.vue
@@ -3,9 +3,31 @@
         <v-layout>
             <v-app-bar v-if="mobile" color="cyan-lighten-4" height="55" :elevation="4">
                 <template v-slot:prepend>
-                    <box-icon name="menu" class="text-md"></box-icon>
+                    <v-btn icon @click="drawer = !drawer"><box-icon name="menu" class="text-md"></box-icon></v-btn> 
                 </template>
             </v-app-bar>
+            <v-navigation-drawer v-if="mobile" v-model="drawer" location="bottom" temporary>
+                <v-list>
+                    <v-container>
+                        <v-toolbar class="rounded-shaped" density="comfortable"
+                            ><template #title><h3 class="title-text">Vi-Chatting</h3></template>
+                        </v-toolbar>
+                    </v-container>
+                    <div class="m-3 border-b-sm"></div>
+                    <v-list-item class="flex flex-column justify-center">
+                        <v-list-item v-for="assistant in assistants" :key="assistant.id">
+                            <router-link :to="`/${assistant.id}`"
+                                ><v-btn
+                                    flat
+                                    class="rounded-md w-[10rem] bg-gradient-to-bl from-cyan-200 to-cyan-400 justify-start"
+                                    >{{ assistant.name }}</v-btn
+                                ></router-link
+                            >
+                        </v-list-item>
+                    </v-list-item>
+                </v-list>
+            </v-navigation-drawer>
+
             <v-navigation-drawer elevation="10" class="bg-cyan-lighten-4">
                 <v-list>
                     <v-container>
@@ -51,7 +73,7 @@ const { isLogin, logOff } = useAuth()
 const mobile = useDisplay().mobile
 const assistants = ref()
 const user = JSON.parse(localStorage.getItem(`loggedUser`) || ``)
-
+const drawer = ref(false)
 onMounted(async () => {
     assistants.value = await getAssistants(user)
 })
